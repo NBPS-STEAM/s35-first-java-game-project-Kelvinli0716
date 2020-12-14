79f6c0534b018459fd86f472c34effedfdec6d52
Kelvin Li <chen.li@mynbps.org>
2020-12-14 16:04:42 +0000

    create user imput

--- App.java
+++ App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+         Scanner input1 = new Scanner(System.in);
+         System.out.print("Enter the temperature: ");
+         String tem = input1.nextLine();
+         Scanner input2 = new Scanner(System.in);
+         System.out.print("Enter the description(windy,cold,drizzle,rain,hurrican,snow,sunny): ");
+         String descirp = input2.nextLine();
+         Scanner input3 = new Scanner(System.in);
+         System.out.print("Enter the sports you are going to do: ");
+         String sports = input3.nextLine();
+        
+        
+        
     }
+
+
+
+    
 }
