6733555a29da449714a382abb48c4b42ff360a1a
Kelvin Li <chen.li@mynbps.org>
2020-12-15 16:33:04 +0000

    fix code

--- App.java
+++ App.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         Scanner input = new Scanner(System.in);
-         String num = input.nextLine();
-         System.out.print("Enter the number (1-10): ")
+
+        System.out.print("Enter the number (1-10): ");
+        int num = input.nextInt();
+         
     }
 }
