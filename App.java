e1fad7e3bca4a97d48c07666f26a2819b7d9130c
Kelvin Li <chen.li@mynbps.org>
2020-12-15 15:36:41 +0000

    rewrite

---
+++ App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+         String num = input.nextLine();
+         System.out.print("Enter the number (1-10): ")
+    }
+}
