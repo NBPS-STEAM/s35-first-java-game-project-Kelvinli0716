d6afd0cde669b8224f842fa4671f15da78944bea
Kelvin Li <chen.li@mynbps.org>
2020-12-15 15:33:32 +0000

    Change my idea of game

---
+++ GuessingGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+public class GuessingGame {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+         String num = input.nextLine();
+         System.out.print("Enter the number (1-10): ");
+        
+        
+        
+    }
+
+
+
+    
+}
