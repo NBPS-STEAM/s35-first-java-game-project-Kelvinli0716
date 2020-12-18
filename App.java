3fd1bd71d49f95a2d926ed16a4fa40f8d0f4f310
Kelvin Li <chen.li@mynbps.org>
2020-12-18 02:21:47 +0000

    if statement

--- App.java
+++ App.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        
 
-        System.out.print("Enter the number (1-10): ");
-        int num = input.nextInt();
-         
-    }
+        
+        Scanner input = new Scanner(System.in);
+        int guessNum;
+        int tries = 0;
+        
+        int correctNum = (int) (Math.random() * 9) + 1; 
+        //(int) (Math.random() * (max - min)) + min;
+        
+        boolean win = false；
+
+        while (win == false) 
+        {
+        System.out.print("Guess a number between 1 to 10");
+        guessNum = input.nextInt();
+        tries ++;
+
+        if (guessNum == correctNum) {
+        win = true;
+        }
+        else if (guessNum == correctNum)
 }
