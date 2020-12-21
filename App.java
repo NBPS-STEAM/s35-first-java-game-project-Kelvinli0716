50a4077153081eddb16a233883a2f246ec78d32d
Kelvin Li <chen.li@mynbps.org>
2020-12-21 00:40:47 +0000

    almost down

--- App.java
+++ App.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         
-
-        
         Scanner input = new Scanner(System.in);
         int guessNum;
         int tries = 0;
@@ -11,16 +9,26 @@ public class App {
         int correctNum = (int) (Math.random() * 9) + 1; 
         //(int) (Math.random() * (max - min)) + min;
         
-        boolean win = false；
+        boolean win = false;
 
-        while (win == false) 
-        {
-        System.out.print("Guess a number between 1 to 10");
-        guessNum = input.nextInt();
-        tries ++;
+        while (win == false) {
+         System.out.print("Guess a number between 1 to 10: ");
+         guessNum = input.nextInt();
+         tries ++;
 
-        if (guessNum == correctNum) {
-        win = true;
+         if (guessNum == correctNum) {
+         win = true;
+         }
+         else if (guessNum <= correctNum) {
+         System.out.println("Your guess is too low!");
+         }
+         else if (guessNum >= correctNum) {
+         System.out.println("Your guess is too high!");
+         }
         }
-        else if (guessNum == correctNum)
+        System.out.println("You Win!");
+        System.out.println("The number was " + correctNum + ".");
+        System.out.println("It took you " + tries + " tries.");
+    }
+
 }
